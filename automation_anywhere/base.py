0a68f7450365025d5cd8b858738118ac7df8a635
Mateus <minterciso@gmail.com>
2019-03-27 15:33:22 -0300

    * It seems there are sometimes a 'cache' for TaskRunDetails rows. So I'm   first getting this cache, then searching on the cache if the task was   executed. The issue is, how to make it work when there's no cache?

--- automation_anywhere/base.py
+++ automation_anywhere/base.py
@@ -61,21 +61,50 @@ class Executor:
             self.__logger.error('Error connecting to database: {err}'.format(err=error))
             raise
 
-    def __get_task_id(self, task: str, client: str):
+    def __get_task_cache(self, task: str, client: str):
         fname = task.split('\\')[-1]
-        query = 'select trd.Id as id from TaskRunDetails trd , Tasks t, Clients c, Users u where ' \
-                'trd.TaskId=t.id and trd.ClientId=c.id and trd.UserId=u.id and ' \
-                'u.UserName=\'{username}\' and c.HostName=\'{client}\' and t.FileName=\'{filename}\' ' \
-                'and trd.IsTaskExecutionCompleted=0 and trd.StartTime is not NULL ' \
-                'order by Id desc'.format(username=self.__username,
-                                          client=client,
-                                          filename=fname
-                                          )
+        self.__logger.debug('Looking for cache for task \'{fname}\''.format(fname=fname))
+        query = 'select trd.Id as id from TaskRunDetails trd, Tasks t, Clients c, Users u where	' \
+                'trd.TaskId=t.id and trd.ClientId=c.id and trd.UserId=u.id and ClientStatus=0 and IsTaskExecutionCompleted=0 and ' \
+                'u.UserName=\'{username}\' and c.HostName=\'{client}\' and t.FileName=\'{filename}\''.format(username=self.__username,
+                                                                                                             client=client,
+                                                                                                             filename=fname
+                                                                                                             )
         try:
-            row = self.__database_conn.execute(query).fetchone()
-            if row is not None:
-                return row['id']
-            return -1
+            row = self.__database_conn.execute(query).fetchall()
+            cache = []
+            for r in row:
+                cache.append(r['id'])
+            if len(cache) > 0:
+                self.__logger.debug('Got cache ids: {cache}'.format(cache=','.join([str(x) for x in cache])))
+                return cache
+            return None
+        except Exception as error:
+            self.__logger.error('Unable to execute query {query}:{err}'.format(query=query, err=error))
+            raise
+
+    def __get_task_id(self, task: str, client: str, cache: list = None):
+        fname = task.split('\\')[-1]
+        if cache is None:
+            query = 'select trd.Id as id from TaskRunDetails trd , Tasks t, Clients c, Users u where ' \
+                    'trd.TaskId=t.id and trd.ClientId=c.id and trd.UserId=u.id and ' \
+                    'u.UserName=\'{username}\' and c.HostName=\'{client}\' and t.FileName=\'{filename}\' ' \
+                    'and trd.IsTaskExecutionCompleted=0 and trd.StartTime is not NULL ' \
+                    'order by Id desc'.format(username=self.__username,
+                                              client=client,
+                                              filename=fname
+                                              )
+        else:
+            query = 'select trd.Id as id from TaskRunDetails trd where ' \
+                    'trd.Id in ({ids}) ' \
+                    'and IsTaskExecutionCompleted=0 and StartTime is not NULL'.format(ids=','.join([str(x) for x in cache]))
+        try:
+            task_id = -1
+            while task_id < 0:
+                row = self.__database_conn.execute(query).fetchone()
+                if row is not None:
+                    task_id = row['id']
+            return task_id
         except Exception as error:
             self.__logger.error('Unable to execute query {query}:{err}'.format(query=query, err=error))
             raise
@@ -84,10 +113,8 @@ class Executor:
         query = 'select trd.Status as status, trd.IsTaskExecutionCompleted as completed, trd.ErrorMessage as error ' \
                 'from TaskRunDetails trd ' \
                 'where trd.Id={t_id}'.format(t_id=task_id)
-        self.__logger.debug('Executing query: {q}'.format(q=query))
         try:
             row = self.__database_conn.execute(query).fetchone()
-            self.__logger.debug('Return = {r}'.format(r=row))
             if row is not None:
                 return {
                     'status': row['status'],
@@ -134,6 +161,10 @@ class Executor:
             'X-Authorization': self.__auth_info['request_token']
         }
         deploy_json = json.dumps(deploy_data)
+        # Get the "cache" before executing the task
+        cache = None
+        if self.__check_status:
+            cache = self.__get_task_cache(task, client)
         try:
             r = requests.post(self.__url + '/' + self.__api['deployment'], data=deploy_json, headers=deploy_headers)
         except Exception as error:
@@ -150,7 +181,7 @@ class Executor:
             self.__logger.info('Getting task status...')
             task_id = -1
             while task_id < 0:
-                task_id = self.__get_task_id(task=task, client=client)
+                task_id = self.__get_task_id(task=task, client=client, cache=cache)
                 time.sleep(1)
             self.__logger.info('Got task ID {t_id}.'.format(t_id=task_id))
             self.__logger.debug('Waiting for task to complete...')
