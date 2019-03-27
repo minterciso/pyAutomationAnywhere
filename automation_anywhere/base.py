574ebe29f2c56a280c502fb7fac1b9cbf8160840
Mateus <minterciso@gmail.com>
2019-03-27 11:00:47 -0300

    * Added some queries for checking the status. Right now it seems that   it's working when the task fails, however sometimes we are unable to   get the task status.

--- automation_anywhere/base.py
+++ automation_anywhere/base.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import logging
+import sqlalchemy
+import time
 
 from . import errors
 
@@ -37,6 +39,76 @@ class Executor:
             'request_text': ''
         }
         self.__logger = logging.getLogger()
+        self.__check_status = False
+        self.database_options = {
+            'hostname': None,
+            'username': None,
+            'password': None
+        }
+        self.__database_conn = None
+
+    def __connect_to_database(self):
+        if self.__database_conn is not None:
+            return
+        try:
+            db_connection = 'mssql+pyodbc://{username}:{password}@{hostname}'.format(
+                username=self.database_options['username'],
+                password=self.database_options['password'],
+                hostname=self.database_options['hostname']
+            )
+            self.__database_conn = sqlalchemy.create_engine(db_connection)
+        except Exception as error:
+            self.__logger.error('Error connecting to database: {err}'.format(err=error))
+            raise
+
+    def __get_task_id(self, task: str, client: str):
+        fname = task.split('\\')[-1]
+        query = 'select trd.Id as id from TaskRunDetails trd , Tasks t, Clients c, Users u where ' \
+                'trd.TaskId=t.id and trd.ClientId=c.id and trd.UserId=u.id and ' \
+                'u.UserName=\'{username}\' and c.HostName=\'{client}\' and t.FileName=\'{filename}\' ' \
+                'and trd.IsTaskExecutionCompleted=0 and trd.StartTime is not NULL ' \
+                'order by Id desc'.format(username=self.__username,
+                                          client=client,
+                                          filename=fname
+                                          )
+        try:
+            row = self.__database_conn.execute(query).fetchone()
+            if row is not None:
+                return row['id']
+            return -1
+        except Exception as error:
+            self.__logger.error('Unable to execute query {query}:{err}'.format(query=query, err=error))
+            raise
+
+    def __get_task_status(self, task_id: int):
+        query = 'select trd.Status as status, trd.IsTaskExecutionCompleted as completed, trd.ErrorMessage as error ' \
+                'from TaskRunDetails trd ' \
+                'where trd.Id={t_id}'.format(t_id=task_id)
+        self.__logger.debug('Executing query: {q}'.format(q=query))
+        try:
+            row = self.__database_conn.execute(query).fetchone()
+            self.__logger.debug('Return = {r}'.format(r=row))
+            if row is not None:
+                return {
+                    'status': row['status'],
+                    'complete': (False if row['completed'] == 0 else True),
+                    'error': row['error']
+                }
+            return None
+        except Exception as error:
+            self.__logger.error('Unable to execute query {query}:{err}'.format(query=query, err=error))
+            raise
+
+    def set_check_status(self, check: bool):
+        if check:
+            self.__connect_to_database()
+            self.__check_status = True
+        else:
+            self.__check_status = False
+            self.__database_conn = None
+
+    def get_check_status(self):
+        return self.__check_status
 
     def deploy_task(self, task: str, client: str):
         """
@@ -74,6 +146,21 @@ class Executor:
             self.__logger.info('Reason: %s', r.reason)
             self.__logger.info('Description: %s', r.text)
             raise errors.Error(r.reason)
+        if self.__check_status:
+            self.__logger.info('Getting task status...')
+            task_id = -1
+            while task_id < 0:
+                task_id = self.__get_task_id(task=task, client=client)
+                time.sleep(1)
+            self.__logger.info('Got task ID {t_id}.'.format(t_id=task_id))
+            self.__logger.debug('Waiting for task to complete...')
+            self.task_status = self.__get_task_status(task_id)
+            while self.task_status['complete'] is False:
+                time.sleep(1)
+                self.task_status = self.__get_task_status(task_id)
+            if self.task_status['status'] == 'Completed':
+                return True
+            return False
         return True
 
     def __authenticate(self):
