c5673256f025ab794a9e6a496d4abeef11f0d41e
Mateus Interciso <minterciso@gmail.com>
2019-03-29 17:18:11 -0300

    * Added the package information on the master (production) version

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="automation_anywhere",
+    version="0.0.1",
+    author="Mateus Interciso",
+    author_email="minterciso@gmail.com",
+    description="A Python Package to deploy tasks on Automation Anywhere v10.5",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/minterciso/pyAutomationAnywhere",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
